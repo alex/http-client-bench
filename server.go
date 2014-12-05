a115120863c51fc613d6fb321f73c9f2926b5e19
Alex Gaynor <alex.gaynor@gmail.com>
2014-12-05 10:04:16 -0800

    Send content-length instead of chunked transfer

--- server.go
+++ server.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 )
 
+// 32 GiB
+var DESIRED_SIZE = 1024 * 1024 * 1024 * 32
 var CHUNK = [1024]byte{}
 
 type FastStreamHandler struct{}
 
 func (f FastStreamHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/octet-stream")
-	for {
+	w.Header().Add("Content-Length", strconv.Itoa(DESIRED_SIZE))
+	for i := 0; i < DESIRED_SIZE/len(CHUNK); i++ {
 		_, err := w.Write(CHUNK[:])
 		if err != nil {
 			break
